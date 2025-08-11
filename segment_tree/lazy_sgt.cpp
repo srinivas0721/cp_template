bd39e989c21deb0ab7d0712dc5e919c2beb3ee22
Srinivas Prabhu <srinivasprabhu557@gmail.com>
2025-08-11 14:20:41 +0530

    zegment tree correction

--- segment_tree/lazy_sgt.cpp
+++ segment_tree/lazy_sgt.cpp
@@ -1,83 +1,87 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 #define int long long
 #define endl '\n' 
-#define INF = LLONG_MAX>>1
-const int MOD = 1e9 +7;
+const int INF = LLONG_MAX >> 1;
+const int MOD = 1e9 + 7;
 
-//perform 2 queries on an array:
-//1 l r x : add x to all the elment form l to r
-//2 l r : return min. of all elment from l to r
+vector<int> segTree, lazy;
 
-
-vector<int>segTree,lazy;
-int merge (int a,int b){
-  return min(a,b);
-}
-void build(vector<int>&arr,int start,int end,int index){
-  if(start ==end){
-    segTree[index]=arr[start];
-    return ;
-  }
-  int mid = (start + end)/2;
-  int left = 2*index,right = 2*index +1;
-  build(arr,start,mid,left);
-  build (arr,mid+1,end,right);
-  segTree[index]=merge(segTree[left]+segTree[right]);
-}
-void propogate(int start,int end,int index){
-  if(start==end){
-    lazy [index]=0;
-    return;
-  }
-  left[2*index] += lazy[index];
-  left[2*index +1]+=lazy[index];
-  lazy[index]=0;
-}
-void update(int start,int end,int index,int l,int r,int value){
-  //check for pending updates in lazy
-  if(lazy[index]!=0){
-    segTree+=lazy[index];
-    propogate(start,end,index);
-  }
-  if(start>=l&&end<=r){
-    segTree[index]+=value;
-    lazy[index]=value;
-    propogate(start,end,index);
-    return;
-  }
-  if(start>r||end<l){
-    return;
-  }
-  int mid = (start+end)/2;
-  update(start,mid,2*index,l,r,value);
-  update(mid+1,end,2*index+1,l,r,value);
-  segTree[index]=merge(segTree[2*index],segTree[2*index+1]);
-}
-int query(int start,int end,int index,int l,int r){
-  if(lazy[index]!=0){
-    segTree[index]+=lazy[index];
-    propogate(start,end,index);
-  }
-  if(start>=l&&end<=r){
-    return segTree[index];
-  }
-  if(l>end||r<start){
-    return LLONG_MAX;
-  }
-  int mid = (start+end)/2;
-  int leftAnswer = query(start,mid,2*index,l,r);
-  int rightAnswer = query(mid+1,end,2*index+1,l,r);
-  return merge(leftAnswer,rightAnswer);
+int merge(int a, int b) {
+    return min(a, b);
 }
 
-signed main (){
-ios::sync_with_stdio(false);cin.tie(NULL);
-int t;
-cin>>t;
-while(t--){
-  
+void build(vector<int>& arr, int start, int end, int index) {
+    if (start == end) {
+        segTree[index] = arr[start];
+        return;
+    }
+    int mid = (start + end) / 2;
+    build(arr, start, mid, 2 * index);
+    build(arr, mid + 1, end, 2 * index + 1);
+    segTree[index] = merge(segTree[2 * index], segTree[2 * index + 1]);
 }
-return 0;
+
+void propagate(int start, int end, int index) {
+    if (lazy[index] != 0) {
+        segTree[index] += lazy[index];
+        if (start != end) { // not a leaf node
+            lazy[2 * index] += lazy[index];
+            lazy[2 * index + 1] += lazy[index];
+        }
+        lazy[index] = 0;
+    }
 }
+
+void update(int start, int end, int index, int l, int r, int value) {
+    propagate(start, end, index);
+    if (start > r || end < l) return; // no overlap
+    if (start >= l && end <= r) {
+        lazy[index] += value;
+        propagate(start, end, index);
+        return;
+    }
+    int mid = (start + end) / 2;
+    update(start, mid, 2 * index, l, r, value);
+    update(mid + 1, end, 2 * index + 1, l, r, value);
+    segTree[index] = merge(segTree[2 * index], segTree[2 * index + 1]);
+}
+
+int query(int start, int end, int index, int l, int r) {
+    propagate(start, end, index);
+    if (start > r || end < l) return INF; // no overlap
+    if (start >= l && end <= r) return segTree[index];
+    int mid = (start + end) / 2;
+    int leftAnswer = query(start, mid, 2 * index, l, r);
+    int rightAnswer = query(mid + 1, end, 2 * index + 1, l, r);
+    return merge(leftAnswer, rightAnswer);
+}
+
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int t; cin >> t;
+    while (t--) {
+        int n, q; cin >> n >> q;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        segTree.assign(4 * n, 0);
+        lazy.assign(4 * n, 0);
+        build(arr, 0, n - 1, 1);
+        while (q--) {
+            int type; cin >> type;
+            if (type == 1) {
+                int l, r, x; cin >> l >> r >> x;
+                // assuming 0-based indexing
+                update(0, n - 1, 1, l, r, x);
+            } else {
+                int l, r; cin >> l >> r;
+                cout << query(0, n - 1, 1, l, r) << endl;
+            }
+        }
+    }
+    return 0;
+}
+// Range updates: Add a value x to every element in a subrange [l, r] of the array efficiently (in O(log n) time).
+
+// Range minimum queries: Find the minimum element in a subrange [l, r] efficiently (in O(log n) time).
